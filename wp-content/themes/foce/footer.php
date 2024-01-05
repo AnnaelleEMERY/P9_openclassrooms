90d4bcb15f0fc996ee84fd24b387356589d8ed4b
Annaelle Emery <annaelleemery512@gmail.com>
2024-01-05 11:20:34 +0100

    Footer oscars added

--- wp-content/themes/foce/footer.php
+++ wp-content/themes/foce/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -11,16 +12,28 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-        <ul>
-            <li><a href="#">Mentions Légales</a></li>
-            <li><a href="#">STUDIO KOUKAKI</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
-	</footer><!-- #colophon -->
+<footer id="colophon" class="site-footer">
+
+    <div class="oscars-div">
+        <div class="oscars-text-div">
+            <p class="oscars-text">Fleurs d'oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</p>
+        </div>
+
+        <div class="oscars-img-div">
+            <img src="<?php echo get_template_directory_uri() . '/assets/images/oscars-logo.png'; ?>" alt="Logo Oscars Short Film Animated">
+        </div>
+    </div>
+
+    <ul>
+        <li><a href="#">Mentions Légales</a></li>
+        <li><a href="#">STUDIO KOUKAKI</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
